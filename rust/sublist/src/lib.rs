c2fd57cd5553b346121aed3983a0a3ec70669a4e
Sivakar Sithamparanathan <sivakar12@outlook.com>
2021-10-04 20:55:16 +0530

    Improve sublist solution

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -8,15 +8,10 @@ pub enum Comparison {
 fn is_sublist<T: PartialEq>(sublist: &[T], superlist: &[T]) -> bool {
     if sublist.len() > superlist.len() { return false; }
     if sublist == [] { return true; }
-    let start_max = superlist.len() - sublist.len();
-    for i in 0..=start_max {
-        if sublist[0] == superlist[i] {
-            if superlist[i..i+sublist.len()] == sublist[..] {
-                return true
-            }
-        }
-    }
-    return false;
+    superlist.windows(sublist.len())
+        .any(|window| {
+            *window == *sublist
+        })
 }
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
     match (is_sublist(_first_list, _second_list), is_sublist(_second_list, _first_list)) {
